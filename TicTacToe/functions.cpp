27e4816bdccf35d2bfaa93802fe941a944ddef48
Jeff Farris <jeff.markues@gmail.com>
2017-03-24 23:37:05 -0400

    fix window name

--- TicTacToe/functions.cpp
+++ TicTacToe/functions.cpp
@@ -19,7 +19,7 @@ bool init() {
 		}
 		
 		// Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		gWindow = SDL_CreateWindow("TicTacToe", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if(gWindow == NULL) {
 			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
 			success = false;
